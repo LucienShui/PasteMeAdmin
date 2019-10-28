0bd4277b9ee30859a345dda87a9a4672237e479a
Lucien Shui <lucien@lucien.ink>
2019-10-28 17:56:19 +0800

    Init

---
+++ src/main/java/cn/pasteme/admin/PastemeAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.pasteme.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastemeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastemeAdminApplication.class, args);
+    }
+
+}
